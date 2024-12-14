129cca981d7452e8f76125829dc86632332b3439
caleb <kalejaiyecaleb@gmail.com>
2024-12-14 23:55:37 +0100

    common

--- backend/middleware/validateUrl.js
+++ backend/middleware/validateUrl.js
@@ -1,5 +1,13 @@
-const validateUrl = (req, res) => {
+const axios = require("axios");
+
+const validateUrl = async(req, res) => {
     const { url } = req.body;
+    try{
+        const response = await axios.head(url);
+        res.status(200).json({ message: 'URL is valid and accessible' });
+    } catch(err){
+        console.log(err)
+    }
 }
 
 module.exports = validateUrl;
