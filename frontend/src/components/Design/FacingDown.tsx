806d8eecaae45d17c8ab3d80e7b19b339c57c671
Caleb Kalejaiye <kalejaiyecaleb@gmail.com>
2025-01-02 00:58:12 +0100

    commit

--- frontend/src/components/Design/FacingDown.tsx
+++ frontend/src/components/Design/FacingDown.tsx
@@ -3,11 +3,11 @@ import { long, longer, longest, medium, small } from "../../images"
 const FacingUp = () => {
   return (
     <div className="absolute top-0 left-0 right-0 m-auto flex flex-col">
-        <img src={small} />
-        <img src={medium} />
-        <img src={long} />
-        <img src={longer} />
         <img src={longest} />
+        <img src={longer} />
+        <img src={long} />
+        <img src={medium} />
+        <img src={small} />
     </div>
   )
 }
