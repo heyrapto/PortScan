59974d068e60203d5c0d6707300f654d87dc5200
Caleb Kalejaiye <kalejaiyecaleb@gmail.com>
2025-01-02 15:59:31 +0100

    commit

--- frontend/src/components/People.tsx
+++ frontend/src/components/People.tsx
@@ -3,13 +3,13 @@ import { person1, person2, person3, person4, person5, person6, person7 } from ".
 const People = () => {
   return (
     <div className="flex mt-[70px] gap-[50px] pb-[20px] ">
-      <img src={person1} className="w-[80px]" />
-      <img src={person2} className="w-[80px]" />
-      <img src={person3} className="w-[80px]" />
-      <img src={person4} className="w-[80px]" />
-      <img src={person5} className="w-[80px]" />
-      <img src={person6} className="w-[80px]" />
-      <img src={person7} className="w-[80px]" />
+      <img src={person1} className="md:w-[80px] w-[50px]" />
+      <img src={person2} className="md:w-[80px] w-[50px]" />
+      <img src={person3} className="md:w-[80px] w-[50px]" />
+      <img src={person4} className="md:w-[80px] w-[50px]" />
+      <img src={person5} className="md:w-[80px] w-[50px]" />
+      <img src={person6} className="md:w-[80px] w-[50px]" />
+      <img src={person7} className="md:w-[80px] w-[50px]" />
     </div>
   );
 };
