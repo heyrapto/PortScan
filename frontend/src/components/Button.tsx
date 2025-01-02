8a9d8e05ac29fbc72b64cddb5bdb6d43d3527ca1
Caleb Kalejaiye <kalejaiyecaleb@gmail.com>
2025-01-02 02:18:35 +0100

    commit

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -7,7 +7,7 @@ interface ButtonProps {
 
 export const Button = ({ tag, className }: ButtonProps) => {
     return(
-        <div className={`text-white flex items-center justify-center cursor-pointer hover:bg-blue-900 hover:duration-200 rounded-lg ${className} border border-white bg-blue-800 w-[80px] h-[30px] border-[0.1px solid #333]`}>
+        <div className={`text-white flex items-center justify-center cursor-pointer hover:bg-blue-900 hover:duration-200 rounded-lg ${className} border border-[#C4BBF4] bg-blue-800 w-[80px] h-[30px] border-[0.1px solid #333]`}>
             <button type="submit" className="text-[0.6rem] p-[8px] bg-transparent">{tag}</button>
         </div>
     )
