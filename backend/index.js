0df417b2c2c3c12accd8871ea34ffbd0d9bd9c04
Caleb Kalejaiye <kalejaiyecaleb@gmail.com>
2025-01-03 16:51:33 +0100

    commit

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,7 @@ const PORT = process.env.PORT || 7000;
 app.use(express.json())
 app.use("/api/scrape", scraperRoutes)
 app.use(cors());
-app.options("api/scrape", cors())
-
+app.options("api/scrape", cors());
 
 app.listen(PORT, () => {
     console.log("Server is running on ", PORT);
