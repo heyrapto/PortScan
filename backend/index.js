ea60fec1cf3cd3f7312552209d720d1a986c5a56
Caleb Kalejaiye <kalejaiyecaleb@gmail.com>
2025-01-03 04:25:04 +0100

    commit

--- backend/index.js
+++ backend/index.js
@@ -7,14 +7,19 @@ const validateURl = require("./middleware/validateUrl");
 const errorHandler = require("./middleware/errorHandler");
 const PORT = 7000 | "https://port-scan-backend.vercel.app";
 
-app.use(cors({
-    origin: 'https://port-scan-ten.vercel.app',
-    allowedHeaders: ['Content-Type'],
-    credentials: false,
-}))
+app.use(
+    cors({
+      origin: "https://port-scan-ten.vercel.app",  // Allow only this frontend domain
+      allowedHeaders: ["Content-Type"],  // Allow specific headers
+      methods: ["GET", "POST", "OPTIONS"], // Allow specific methods (OPTIONS is for preflight request)
+      credentials: false,  // Set to true if you're using cookies or authentication tokens
+    })
+  );
+app.options("*", cors());
 app.use(express.json())
 app.use("/api/scrape", scraperRoutes)
 
 app.listen(PORT, () => {
     console.log("Server is running on ", PORT);
 })
+
