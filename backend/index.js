2fbda5e0c5a809d77f0e259c21d1e4e40d7662f4
Caleb Kalejaiye <kalejaiyecaleb@gmail.com>
2025-01-03 05:03:14 +0100

    ck

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
 const scraperRoutes = require("./routes/scraperRoutes");
-const PORT = 7000 | "https://port-scan-backend.vercel.app";
+const cors = require('cors');
+const PORT = 7000 || "https://port-scan-backend.vercel.app";
 
 app.use(express.json())
 app.use("/api/scrape", scraperRoutes)
+app.use(cors({
+  origin: "https://port-scan-ten.vercel.app",  // Allow local frontend (or other domains you're working with)
+  methods: ["GET", "POST"],  // Adjust based on your needs
+  allowedHeaders: ["Content-Type"],  // Headers your app might need
+}));
+
 
 app.listen(PORT, () => {
     console.log("Server is running on ", PORT);
