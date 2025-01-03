ccd0fbb356172bc8557d707c0d5f3ead894c4628
Caleb Kalejaiye <kalejaiyecaleb@gmail.com>
2025-01-03 19:20:38 +0100

    ck

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: (origin, callback) => {
       if(!origin || allowedOrigins.includes(origin))  {
-        callback(null || true);
+        callback(null, true);
       } else {
         callback(new Error("Not allowed by CORS"));
       }
