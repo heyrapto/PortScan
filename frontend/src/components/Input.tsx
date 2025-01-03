7f1f13a248620519c444c18aaebf6bc139c44d91
Caleb Kalejaiye <kalejaiyecaleb@gmail.com>
2025-01-03 19:07:37 +0100

    commit

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -32,7 +32,7 @@ export const Input = ({ placeholder, type }: InputProps) => {
     try {
       // Add headers to the axios request
       const result = await axios.post(
-        "https://portscan-clhm/api/scrape.onrender.com",
+        "https://portscan-clhm.onrender.com/api/scrape",
         { url },
         {
           headers: {
