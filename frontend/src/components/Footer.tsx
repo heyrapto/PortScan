65521baf89c619fd048104912e9d06d937cd7be5
Caleb Kalejaiye <kalejaiyecaleb@gmail.com>
2025-01-05 00:48:33 +0100

    commit

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,9 +1,9 @@
 export const Footer = () => {
     return(
         <section className="flex justify-between w-full items-center px-5 pt-5">
-            <a href="" className="md:text-[0.6rem] text-gray-600 text-[0.5rem]">Designed by Kems Design</a>
+            <a href="https://x.com/kemsdesigns" className="md:text-[0.6rem] text-gray-600 text-[0.5rem]">Designed by Kems Design</a>
             <div>
-            <a href="" className="md:text-[0.6rem] text-gray-600 text-[0.5rem]">Built by Rapto</a>
+            <a href="https://x.com/heyrapto" className="md:text-[0.6rem] text-gray-600 text-[0.5rem]">Built by Rapto</a>
             </div>
         </section>
     );
