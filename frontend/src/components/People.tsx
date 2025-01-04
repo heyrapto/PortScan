27262d3be99dad7537c7e257f0829f8e8d125fa0
Caleb Kalejaiye <kalejaiyecaleb@gmail.com>
2025-01-04 05:01:53 +0100

    ck

--- frontend/src/components/People.tsx
+++ frontend/src/components/People.tsx
@@ -3,14 +3,14 @@ import { person1, person2, person3, person4, person5, person6, person7 } from ".
 const People = () => {
   return (
     <div className="flex mt-[70px] py-[25px] relative w-full">
-      <div className="flex items-center justify-center bottom-0 left-0 gap-[50px] absolute overflow-hidden right-0 m-auto">
-      <img src={person1} className="md:w-[80px] w-[50px] " />
-      <img src={person2} className="md:w-[80px] w-[50px] " />
-      <img src={person3} className="md:w-[80px] w-[50px] " />
-      <img src={person4} className="md:w-[80px] w-[50px] " />
-      <img src={person5} className="md:w-[80px] w-[50px] " />
-      <img src={person6} className="md:w-[80px] w-[50px] " />
-      <img src={person7} className="md:w-[80px] w-[50px] " />
+      <div className="flex items-center justify-center bottom-0 left-0 gap-[100px] absolute overflow-hidden right-0 m-auto">
+      <img src={person1} className="w-[50px] " />
+      <img src={person2} className="w-[50px] " />
+      <img src={person3} className="w-[50px] " />
+      <img src={person4} className="w-[50px] " />
+      <img src={person5} className="w-[50px] " />
+      <img src={person6} className="w-[50px] " />
+      <img src={person7} className="w-[50px] " />
       </div>
     </div>
   );
