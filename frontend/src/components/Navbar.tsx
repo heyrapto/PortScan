86539a18e7236ce956b9662631e44aaa429ffaa8
Caleb Kalejaiye <kalejaiyecaleb@gmail.com>
2025-01-05 22:24:16 +0100

    ck

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -15,9 +15,9 @@ export const Navbar = () => {
 
             <div 
                 className={`${
-                    openNavigation ? "flex flex-col bg-blur-xl top-0 z-50 h-screen py-[50px] px-[30px]" : "hidden"
+                    openNavigation ? "flex flex-col bg-blur-xl top-0 z-50 h-screen py-6 px-12" : "hidden"
                 } md:flex md:flex-row md:gap-10 transition-all duration-300 ease-in-out transform md:static absolute top-0 left-0 w-full md:w-auto`}>
-                <ul className="flex md:flex-row md:gap-10 flex-col bg-transparent">
+                <ul className="flex md:flex-row md:gap-10 flex-col bg-transparent pt-12">
                     <div className="flex gap-1">
                         <HomeIcon className="text-gray-400 w-[15px] md:flex hidden" />
                         <li><a href="#" className="text-gray-500 md:text-[0.7rem] text-[1rem] hover:text-gray-300 transition ease-in-out duration-100">Home</a></li>
@@ -33,7 +33,7 @@ export const Navbar = () => {
                 </ul>
                 <Button className="md:hidden flex w-full mt-[20px]" tag={"Analyze"} />
                 <X 
-                className="w-[30px] text-gray-600 cursor-pointer md:hidden flex absolute top-[20px] right-[20px]"
+                className="w-[30px] text-gray-600 cursor-pointer md:hidden flex absolute top-[40px] right-[40px]"
                 onClick={toggleNavigation} 
             />
             </div>
