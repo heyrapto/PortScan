33e1ef4126976cec0bee6ae17136229f0a47a986
Caleb Kalejaiye <kalejaiyecaleb@gmail.com>
2025-08-14 18:57:32 +0100

    hi

--- backend/server.js
+++ backend/server.js
@@ -3,34 +3,25 @@ const app = express();
 const scraperRoutes = require("./routes/scraperRoutes");
 const cors = require('cors');
 const dotenv = require('dotenv');
-dotenv.config();
-
 const logger = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
 const rateLimiter = require("./middleware/rateLimiter");
+dotenv.config();
 
-const PORT = process.env.PORT || 7000;
-
-const allowedOrigins = [
-  "https://port-scan-ten.vercel.app",
-  "https://port-scan-8bl6.onrender.com",
-  "http://localhost:5173",
-];
+const PORT = 7000;
 
 // Middlewares
-app.use(cors({ origin: allowedOrigins }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger);
 app.use(rateLimiter);
 
-// Routes
 app.use("/api/scrape", scraperRoutes);
 app.get("/", (req, res) => {
   res.send("API is working!");
 });
 
-// Error handler (must be last)
 app.use(errorHandler);
 
 app.listen(PORT, () => {
